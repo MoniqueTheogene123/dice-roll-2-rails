00822644fc19a396581efb8d3bee4d675e91d8e5
MoniqueTheogene123 <39348407+MoniqueTheogene123@users.noreply.github.com>
2025-10-09 18:47:12 +0000

    all views complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,21 @@
 class PagesController < ApplicationController
   def home
-    
     render({ :template => "pages_templates/home" })
+  end
+
+  def two_six
+    render({ :template => "pages_templates/two_six"})
+  end
+
+  def five_four
+    render({ :template => "pages_templates/five_four"})
+  end
+
+  def one_twenty
+    render({ :template => "pages_templates/one_twenty"})
+  end
+
+  def two_ten
+    render({ :template => "pages_templates/two_ten"})
   end
 end
